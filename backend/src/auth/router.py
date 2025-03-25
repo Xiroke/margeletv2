a6bbbded3155751382c83d3f59b6669fcaacd52a
xiroke <xiroke2509@gmail.com>
2025-03-25 18:24:52 +0500

    base init fastapi-users

---
+++ backend/src/auth/router.py
@@ -0,0 +1,40 @@
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.routing import APIRouter
+
+from src.db.database import User
+from src.auth.schemas import UserCreate, UserRead, UserUpdate
+from src.auth.users import auth_backend, current_active_user, fastapi_users
+
+
+router = APIRouter(prefix="/", tags=["auth"])
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@router.get("/authenticated-route")
+async def authenticated_route(user: Annotated[User, Depends(current_active_user)]):
+    return {"message": f"Hello {user.email}!"}
--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -1,17 +1,35 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from config import DB_URL
+from typing import AsyncGenerator, Annotated
+
+from fastapi import Depends
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+
+from config import settings
 
 
 class Base(DeclarativeBase):
     pass
 
 
-engine = create_async_engine(DB_URL)
-
+engine = create_async_engine(settings.DB_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_async_session():
-    with async_session_maker() as session:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
         yield session
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    pass
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_user_db(session: Annotated[AsyncSession, Depends(get_async_session)]):
+    yield SQLAlchemyUserDatabase(session, User)
