ef138f620617b54429896890a5734b442944c5c7
xiroke <xiroke2509@gmail.com>
2025-12-01 15:57:15 +0500

    edit README

--- README.md
+++ README.md
@@ -1,42 +1,60 @@
-# Стек
+### Margelet
 
-## Фронтенд
+это мессенджер, доступный в виде веб-сайта, десктопного и мобильного приложения
 
+#### Стек
+
+##### Фронтенд
+
+bun (аналог npm)
 react (vite)
-storybook (Работа с компонентами)
+tanstack router
+tanstack query
+tailwindcss
 lucide (Иконки)
 
-## Бэкэнд
+##### Бэкэнд
 
+uvicorn (аналог pip)
 fastapi
 sqlalchemy (postgres)
 beanie (mongodb)
 
-# Про токены
+#### Про токены
 
 Токены:
 
 - Refresh токен хранится в бд и устанавливается бэкэндом в куки клиента при запросе в эндпоинт login
-- Access токен - jwt токен, который используется при обращении к эндпоинтам, позволяет делать меньше запросов к бд
+- Access токен - jwt токен, который используется при обращении к эндпоинтам, позволяет делать меньше запросов к бд, управляется автоматически с помощью service-worker
 
-## Стилизация - ограничения
+#### Стилизация - ограничения
 
 Названия
-Общие
+
+**Общие**
 
 - Названия файлов и папок в стиле snake_case
 
-frontend:
+**frontend**:
 
 - Компоненты называется в стиле PascalCase
 - Названия css стилей в стиле snake_case
 
-backend:
+**backend**:
 
 - в основном согласно PEP8
-- переменный созданные с помощью Depends - "SomeValueDep"
+- Depends перменные - "SomeValueDep"
 
-## Прочее
+#### Прочее
 
 Caddyfile - необходимо указывать домен/ip
 есть 2 env файла в /, /frontend
+
+##### Как устроена работа с сообщениями
+
+![фото этапов прохождения сообщения](./backend/docs/messagePipline.png)
+
+##### ERD
+
+![ERD postgres](./backend/docs/docs_tools/postgres_diagram.png)
+![ERD mongo](./backend/docs/docs_tools/schema_mongo_db.png)
--- backend/tests/endpoints/conftest.py
+++ backend/tests/endpoints/conftest.py
@@ -2,14 +2,12 @@ import sys
 
 import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession
 
 sys.path.append("./")
 
 from config import settings
 from main import app
 from src.core.db.database import Base, async_session_maker, engine
-from src.core.db.models import GroupModel
 
 assert settings.TEST_MODE
 
@@ -21,7 +19,7 @@ async def client() -> AsyncClient:
         yield c
 
 
-@pytest_asyncio.fixture(scope="function", autouse=True)
+@pytest_asyncio.fixture(scope="function")
 async def setup_database():
     async with engine.connect() as conn:
         await conn.run_sync(Base.metadata.drop_all)
@@ -39,56 +37,3 @@ async def setup_database():
 async def session(setup_database):
     async with async_session_maker() as session:
         yield session
-
-
-@pytest_asyncio.fixture(scope="function")
-async def authorization(session: AsyncSession):
-    group = GroupModel(title="Test group")
-    session.add(group)
-    await session.flush()
-    await session.refresh(group)
-
-    yield group
-
-
-@pytest_asyncio.fixture(scope="function")
-async def group(session: AsyncSession):
-    group = GroupModel(title="Test group")
-    session.add(group)
-    await session.flush()
-    await session.refresh(group)
-
-    yield group
-
-
-# @pytest_asyncio.fixture(scope="function")
-# async def s3():
-#     s3 = s3_bucket_service_factory()
-#     yield s3
-
-
-# @pytest_asyncio.fixture(scope="function")
-# async def chat(session: AsyncSession, group: GroupModel):
-#     chat = ChatModel(title="Test chat", group_id=group.id)
-#     session.add(chat)
-#     await session.flush()
-#     await session.refresh(chat)
-#     yield chat
-
-
-# @pytest_asyncio.fixture(scope="function")
-# async def personal_chat(session: AsyncSession):
-#     personal_chat = PersonalChatModel(title="Test personal chat")
-#     session.add(personal_chat)
-#     await session.flush()
-#     await session.refresh(personal_chat)
-#     yield personal_chat
-
-
-# @pytest_asyncio.fixture(scope="function")
-# async def role_group(session: AsyncSession, group: GroupModel):
-#     role_group = RoleModel(title="Test role group", group_id=group.id)
-#     session.add(role_group)
-#     await session.flush()
-#     await session.refresh(role_group)
-#     yield role_group
